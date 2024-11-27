0f1b153324e5909c4563114effbe51222691feac
Savva Nasyrov <vasyanasyrov06288@gmail.com>
2024-11-27 21:57:19 +0500

    Add cookieManager

---
+++ SescApp/SescApp.Shared/wwwroot/js/cookieManager.js
@@ -0,0 +1,28 @@
+﻿const setCookie = function setCookie(name, value, days, secure = false, sameSite = 'Strict') {
+    if (value === undefined) {
+        console.warn(`Cookie "${name}" не установлен, так как значение не задано.`);
+        return;
+    }
+
+    let expires = "";
+    if (days) {
+        const date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+
+    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/" +
+        "; SameSite=" + sameSite +
+        (secure ? "; Secure" : "");
+}
+
+const getCookie = function getCookie(cookieName) {
+    let cookies = document.cookie.split(";").map(cookie => {
+        let [key, value] = cookie.trim().split("=");
+        return [key, value];
+    });
+
+    let target = cookies.find(([key]) => key === cookieName);
+
+    return target ? target[1] : null;
+}
