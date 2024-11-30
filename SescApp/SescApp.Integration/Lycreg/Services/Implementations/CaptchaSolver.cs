5df1838ae6575e625e1083b5ab1d47028b4ee09b
Савва Насыров <vasyanasyrov06288@gmail.com>
2024-11-30 19:05:31 +0500

    CaptchaSolver is working

--- SescApp/SescApp.Integration/Lycreg/Services/Implementations/CaptchaSolver.cs
+++ SescApp/SescApp.Integration/Lycreg/Services/Implementations/CaptchaSolver.cs
@@ -8,7 +8,7 @@ public class CaptchaSolver(HttpClient httpClient, IConfiguration config) : ICapt
 {
     private readonly string _lycregSource = config["Paths:Lycreg"] ?? throw new KeyNotFoundException("Paths:Lycreg");
 
-    private static readonly Dictionary<(BigInteger, BigInteger), int> COLUMNS_PAIRS = new Dictionary<(BigInteger, BigInteger), int>
+    private static readonly Dictionary<(long, long), int> COLUMNS_PAIRS = new()
     {
         {(524287, 458759), 0}, {(24579, 49155), 0}, {(7, 131071), 1}, {(415, 111), 1},
         {(126983, 258079), 2}, {(24591, 57371), 2}, {(519935, 462343), 3}, {(115459, 99075), 3},
@@ -17,13 +17,12 @@ public class CaptchaSolver(HttpClient httpClient, IConfiguration config) : ICapt
         {(524287, 462343), 8}, {(27, 15), 8}, {(459207, 459143), 9}, {(57731, 49347), 9}
     };
 
-    private static readonly Dictionary<BigInteger, BigInteger> NUM2I = new ()
+    private static readonly Dictionary<long, int> NUM2I = new()
     {
-        {0, 0}, {1, 0}, {2, 1}, {4, 2}, {8, 3}, {16, 4}, {32, 5}, {64, 6}, {128, 7},
-        {256, 8}, {512, 9}, {1024, 10}, {2048, 11}, {4096, 12}, {8192, 13}, {16384, 14},
-        {32768, 15}, {65536, 16}, {131072, 17}, {262144, 18}, {524288, 19}, {1048576, 20},
-        {2097152, 21}, {4194304, 22}, {8388608, 23}, {16777216, 24}, {33554432, 25},
-        {67108864, 26}, {134217728, 27}, {268435456, 28}, {536870912, 29}, {1073741824, 30},
+        {0, 0}, {1, 0}, {2, 1}, {4, 2}, {8, 3}, {16, 4}, {32, 5}, {64, 6}, {128, 7}, {256, 8}, {512, 9},
+        {1024, 10}, {2048, 11}, {4096, 12}, {8192, 13}, {16384, 14}, {32768, 15}, {65536, 16}, {131072, 17},
+        {262144, 18}, {524288, 19}, {1048576, 20}, {2097152, 21}, {4194304, 22}, {8388608, 23}, {16777216, 24},
+        {33554432, 25}, {67108864, 26}, {134217728, 27}, {268435456, 28}, {536870912, 29}, {1073741824, 30},
         {2147483648, 31}
     };
 
@@ -34,8 +33,6 @@ public class CaptchaSolver(HttpClient httpClient, IConfiguration config) : ICapt
         token.ThrowIfCancellationRequested();
 
         var captchaSolution = SolveCaptcha(captchaBytes);
-
-        await File.WriteAllTextAsync("D:\\Downloads\\фигня\\t.txt", "solution: " + captchaSolution, token);
 
         return (captchaId, captchaSolution);
     }
@@ -54,8 +51,6 @@ public class CaptchaSolver(HttpClient httpClient, IConfiguration config) : ICapt
 
         var captchaId = response.Headers.GetValues("X-Cpt").First();
 
-        await File.WriteAllBytesAsync("D:\\Downloads\\фигня\\img.png", captchaBytes, token);
-
         return (captchaBytes, captchaId);
     }
 
@@ -63,24 +58,18 @@ public class CaptchaSolver(HttpClient httpClient, IConfiguration config) : ICapt
     {
         var data = captchaBytes.Skip(104).SkipLast(20).ToArray();
 
-        var numbers = new List<BigInteger>();
+        var numbers = new List<long>();
 
         for (int i = 0; i < 121; i++)
         {
-            byte[] subArray = data.Skip(i).Take(3630 - i).Where((b, index) => index % 121 == 0).Select(x =>
-            {
-                if (x == 0x00)
-                    return (byte)0x30;
-                else if (x == 0x01)
-                    return (byte)0x31;
-                else
-                    return x;
-            }).ToArray();
+            byte[] subArray = data.Skip(i).Take(3630 - i).Where((b, index) => index % 121 == 0).ToArray();
 
-            numbers.Add(new BigInteger(subArray));
+            var number = Convert.ToInt64(new string(subArray.Select(x => x == 0 ? '0' : '1').ToArray()), 2);
+
+            numbers.Add(number);
         }
 
-        var columns = numbers.Select(x => x / (x & -x)).ToArray();
+        var columns = numbers.Select(x => x >> NUM2I[x & -x]).ToArray();
 
         var solution = new StringBuilder();
         var waitFor0 = false;
@@ -93,14 +82,14 @@ public class CaptchaSolver(HttpClient httpClient, IConfiguration config) : ICapt
             {
                 waitFor0 = false;
             }
-            else if (COLUMNS_PAIRS.ContainsKey(pair))
+            else if (COLUMNS_PAIRS.TryGetValue(pair, out int value))
             {
-                solution.Append(COLUMNS_PAIRS[pair]);
+                solution.Append(value);
                 waitFor0 = true;
             }
         }
 
-        return solution.ToString(); 
+        return solution.ToString();
     }
 }
 
