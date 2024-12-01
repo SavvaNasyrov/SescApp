613a3c393f1ebd68de199de431fbb46889e1142b
Савва Насыров <vasyanasyrov06288@gmail.com>
2024-12-01 20:39:21 +0500

    TeachList handled

---
+++ SescApp/SescApp.Integration/Lycreg/Models/MediatR/TeachListRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace SescApp.Integration.Lycreg.Models.MediatR
+{
+    public class TeachListRequest : IRequest<IReadOnlyDictionary<string, string>>
+    {
+        public required Authorization Auth { get; init; }
+    }
+}
