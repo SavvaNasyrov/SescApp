373c6f19c4c017f23d9dc2d4fec289e427b274b6
reveek <sochnevde09@gmail.com>
2024-12-03 09:47:23 +0500

    add caching

--- SescApp/SescApp.Integration/Lycreg/Services/MediatR/SubjListRequestHandler.cs
+++ SescApp/SescApp.Integration/Lycreg/Services/MediatR/SubjListRequestHandler.cs
@@ -2,14 +2,16 @@
 using Microsoft.Extensions.Configuration;
 using SescApp.Integration.Lycreg.Models.MediatR;
 using System.Net.Http.Json;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace SescApp.Integration.Lycreg.Services.MediatR
 {
-    public class SubjListRequestHandler(HttpClient httpClient, IConfiguration config) : IRequestHandler<SubjListRequest, IReadOnlyDictionary<string, string>>
+    public class SubjListRequestHandler(HttpClient httpClient, IConfiguration config, IMemoryCache cache) : IRequestHandler<SubjListRequest, IReadOnlyDictionary<string, string>>
     {
         private readonly string _lycregSource = config["Paths:Lycreg"] ?? throw new KeyNotFoundException("Paths:Lycreg");
 
-        public async Task<IReadOnlyDictionary<string, string>> Handle(SubjListRequest request, CancellationToken cancellationToken)
+        private async Task<IReadOnlyDictionary<string, string>> FetchSubjListAsync(SubjListRequest request,
+            CancellationToken cancellationToken)
         {
             var subjListRequest = new
             {
@@ -26,9 +28,21 @@ namespace SescApp.Integration.Lycreg.Services.MediatR
             resp.EnsureSuccessStatusCode();
 
             var result = await resp.Content.ReadFromJsonAsync<Dictionary<string, string>>(cancellationToken)
-                ?? throw new InvalidOperationException("Response is not dict, may be auth problems");
+                         ?? throw new InvalidOperationException("Response is not dict, may be auth problems");
 
             return result.AsReadOnly();
+        }
+        
+        public async Task<IReadOnlyDictionary<string, string>> Handle(SubjListRequest request, CancellationToken cancellationToken)
+        {
+            if (!cache.TryGetValue("subjList", out IReadOnlyDictionary<string, string>? subjList))
+            {
+                subjList = await FetchSubjListAsync(request, cancellationToken);
+                var cacheEntryOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromDays(1));
+                cache.Set("subjList", subjList, cacheEntryOptions);
+            }
+
+            return subjList!;
         }
     }
 }
