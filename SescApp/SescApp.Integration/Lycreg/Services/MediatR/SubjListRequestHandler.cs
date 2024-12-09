d8992aa72a649e6aa17afb2722a1789bebf41df9
reveek <sochnevde09@gmail.com>
2024-12-09 19:54:35 +0500

    add basedSubjects

--- SescApp/SescApp.Integration/Lycreg/Services/MediatR/SubjListRequestHandler.cs
+++ SescApp/SescApp.Integration/Lycreg/Services/MediatR/SubjListRequestHandler.cs
@@ -34,8 +34,38 @@ namespace SescApp.Integration.Lycreg.Services.MediatR
 
             var result = await resp.Content.ReadFromJsonAsync<Dictionary<string, string>>(cancellationToken)
                          ?? throw new InvalidOperationException("Response is not dict, may be auth problems");
-
-            return result.AsReadOnly();
+            var basedSubjects = new Dictionary<string, string>(){
+                { "s110", "Русский язык" },
+                { "s120", "Литература" },
+                { "s210", "Английский язык" },
+                { "s220", "Немецкий язык" },
+                { "s230", "Французский язык" },
+                { "s310", "Искусство" },
+                { "s320", "МХК" },
+                { "s330", "Музыка" },
+                { "s410", "Математика" },
+                { "s420", "Алгебра" },
+                { "s430", "Алгебра и начала анализа" },
+                { "s440", "Геометрия" },
+                { "s450", "Вероятность и статистика" },
+                { "s460", "Информатика" },
+                { "s510", "История" },
+                { "s520", "История России" },
+                { "s530", "Всеобщая история" },
+                { "s540", "Обществознание" },
+                { "s550", "Экономика" },
+                { "s560", "Право" },
+                { "s570", "География" },
+                { "s610", "Физика" },
+                { "s620", "Астрономия" },
+                { "s630", "Химия" },
+                { "s640", "Биология" },
+                { "s710", "Технология" },
+                { "s810", "Физическая культура" },
+                { "s820", "ОБЗР" }
+            };
+            var combinedResult = result.Concat(basedSubjects).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            return combinedResult.AsReadOnly();
         }
 
         public async Task<IReadOnlyDictionary<string, string>> Handle(SubjListRequest request, CancellationToken cancellationToken)
